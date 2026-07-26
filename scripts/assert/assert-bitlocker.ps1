5c7080d0824eee1adc104361131641864b4c416e
Andres Maqueo <193985782+AndresMaqueo@users.noreply.github.com>
2026-07-26 03:44:17 -0600

    refactor: use result factory in BitLocker assertion

--- scripts/assert/assert-bitlocker.ps1
+++ scripts/assert/assert-bitlocker.ps1
@@ -1,12 +1,14 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
+$resultModulePath = Join-Path (Split-Path -Parent $PSScriptRoot) 'lib/Result.psm1'
+Import-Module -Name $resultModulePath -Force -ErrorAction Stop
+
 $bitLockerVolume = Get-BitLockerVolume -MountPoint 'C:'
 $protectionStatus = $bitLockerVolume.ProtectionStatus
 
-[pscustomobject][ordered]@{
-    ControlId = 'BL-001'
-    Actual    = [string]$protectionStatus
-    Pass      = ($protectionStatus -eq [Microsoft.BitLocker.Structures.BitLockerVolumeProtectionStatus]::On)
-    Message   = "BitLocker protection status for C: is '$protectionStatus'."
-}
+New-ControlResult `
+    -ControlId 'BL-001' `
+    -Actual ([string]$protectionStatus) `
+    -Pass ($protectionStatus -eq [Microsoft.BitLocker.Structures.BitLockerVolumeProtectionStatus]::On) `
+    -Message "BitLocker protection status for C: is '$protectionStatus'."
